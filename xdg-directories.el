e6fae2ed8fdb6af95f317c0754468407ab8fa2e7
Francisco Miguel Colaço <francisco.colaco@gmail.com>
2016-09-07 17:16:15 +0100

    Corrected documentation strings and functions API.
    Documentation strings were corrected and disambiguated.
    
    A parameter named PATH and another named FILENAME were renamed to NAME,
    to make it similar to the parameters of 'expand-file-name'.

--- xdg-directories.el
+++ xdg-directories.el
@@ -33,81 +33,98 @@
 
 (defgroup xdg-directories
     ()
-  "XDG Directory Specification"
+  "XDG Base Directory Specification"
   :group 'environment
   :link '(emacs-library-link "xdg-directories"))
 
 
-(defun xdg-get-path (domain &optional path)
-  "Return the directory at DOMAIN, with an optional PATH relative to it."
+(defun xdg-get-path (domain &optional name)
+  "Return the directory at DOMAIN, with an optional NAME relative to it."
   (let ((dir (s-chomp (shell-command-to-string (concat "xdg-user-dir " domain)))))
-    (if path
-        (expand-file-name path dir)
+    (if name
+        (expand-file-name name dir)
         dir)))
 
 
 (defcustom xdg-data-home
   (or (getenv "XDG_DATA_HOME") (expand-file-name "~/.local/share"))
-  "The base directory relative to which user specific data files
-  should be stored."
+  "Directory to store data files.
+
+This directory stores data files for application."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom xdg-config-home
   (or (getenv "XDG_CONFIG_HOME") (expand-file-name "~/.config"))
-  "The base directory relative to which user specific
-  configuration files should be stored."
+  "Directory to store configuration files.
+
+This directory stores files that configure applications.  This
+directory should not contain machine generated session files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom xdg-cache-home
   (or (getenv "XDG_CACHE_HOME") (expand-file-name "~/.cache"))
-  "The base directory relative to which user specific
-  non-essential data files should be stored."
+  "Directory to store non essential files.
+
+In this directory, user specific non essential data files are
+stored.  These files should not contain information that cannot
+be regenerated, in case they are erased."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom xdg-runtime-dir
   (getenv "XDG_RUNTIME_DIR")
-  "The directory where files that pertain only to this session
-  are stored.  These files can be erased when the application quits
-  or the user logs out."
+  "Directory to store temporary files.
+
+In this directory, files that pertain only to this session are
+stored.  These files can be erased when the application quits
+or the user logs out."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-emacs-data-home
   (expand-file-name "emacs" xdg-data-home)
-  "The base directory relative to which user Emacs specific data
-  files should be stored."
+  "Directory to store user Emacs data files.
+
+This directory stores data files for Emacs and it's
+components or packages."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-emacs-config-home
   (expand-file-name "emacs" xdg-config-home)
-  "The base directory relative to which user Emacs specific
-  configuration files should be stored."
+  "Directory to store user Emacs configuration files.
+
+This directory stores configuration files for Emacs and it's
+components or packages, and should not contain machine
+generated session files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-emacs-cache-home
   (expand-file-name "emacs" xdg-cache-home)
-  "The base directory relative to which user Emacs specific
-  non-essential data files should be stored."
+  "Directory to store user Emacs cache files.
+
+This directory stores non essential data files for Emacs and
+it's components or packages, and should not contain any files
+that would cause loss of information when erased."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-emacs-runtime-dir
   (expand-file-name "emacs" xdg-runtime-dir)
-  "The directory where Emacs files that pertain only to this session
-  are stored.  These files can be erased when the application quits
-  or the user logs out."
+  "Directory to store user Emacs temporary files.
+
+This directory stores files that may be erased when the user
+logs out."
   :group 'xdg-directories
   :type 'directory)
 
@@ -121,60 +138,74 @@
 
 (defcustom user-download-directory
   (xdg-get-path "DOWNLOAD")
-  "The directory where the user stores his downloaded files by default."
+  "The downloads directory.
+
+This is the default directory where the user stores transferred files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-templates-directory
   (xdg-get-path "TEMPLATES")
-  "The directory where the user stores his templates."
+  "The templates directory.
+
+This is the default directory where the user stores template files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-public-share-directory
   (xdg-get-path "PUBLICSHARE")
-  "The directory where the user stores his shared files."
+  "The public share directory.
+
+This is a directory that is shared with other users."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-documents-directory
   (xdg-get-path "DOCUMENTS")
-  "The directory where the user stores his documents."
+  "The documents directory.
+
+This is the directory where the user stores documents."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-music-directory
   (xdg-get-path "MUSIC")
-  "The directory where the user stores his music."
+  "The music directory.
+
+This is the directory where the user stores music and sound files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-pictures-directory
   (xdg-get-path "PICTURES")
-  "The directory where the user stores his pictures."
+  "The pictures directory.
+
+This is the directory where the user stores pictures and image files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defcustom user-videos-directory
   (xdg-get-path "VIDEOS")
-  "The directory where the user stores his videos."
+  "The video directory.
+
+  This is the directory where the user stores video files."
   :group 'xdg-directories
   :type 'directory)
 
 
 (defun xdg-get-user-file (filename directory &optional create-parent-dirs)
-  "Locate FILENAME under DIRECTORY, wherther it exists or not.
+  "Locate NAME under DIRECTORY, wherther it exists or not.
 
 If CREATE-PARENT-DIRS is T, the parent directories are created.
 
 Returns the complete file path, so it can be chained."
-  (let ((file (expand-file-name filename directory)))
+  (let ((file (expand-file-name name directory)))
     ;; Create any parent directories, if so requested.
     (if create-parent-dirs
       (unless (file-exists-p directory)
@@ -234,13 +265,8 @@ dirs will be created in case they are not found."
 (defun locate-user-document-file (filename)
   "Locate a document file named FILENAME.
 
-The file may exist or not under the user Emacs documents
-dir (which is predetermined by this package using operating
-system tools or, if found lacking, sensible defaults).  The path
-of the file is returned regardless of it's existence.
-
-If CREATE-PARENT-DIRS is t, then the directory and its parent
-dirs will be created in case they are not found."
+The file resides under 'user-documents-directory' and may exist
+or not."
   (expand-file-name filename user-documents-directory))
 
 
